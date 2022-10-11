abcb9002b09af210efc6189b9cf373ce28e82e7c
Nisa <nisayurdagul385@gmmail.com>
2022-10-11 22:33:35 +0300

    kosullar

---
+++ kosullar.js
@@ -0,0 +1,24 @@
+//if else
+ var name='Nisa';
+ var age=21;
+ var drivingLicence=false;
+
+ if(name =='Nisa')
+ {
+    console.log('kod calisti');
+ }
+
+ if(age==20){
+    console.log('yasiniz  20 değil');
+ }
+ else{
+    console.log('yasinizz 21 olabilir');
+ }
+
+ if(drivingLicence ==true)
+ {
+    console.log('arac kullanabilirsiniz');
+ }
+ else{
+    console.log('yasiniz 18 den kucuk oldugu icin arac kullanamazsiniz');
+ }
