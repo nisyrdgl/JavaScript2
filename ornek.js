1f30ce592238b42b32cebd61096431642bb0cb2f
Nisa <nisayurdagul385@gmmail.com>
2022-10-11 23:39:53 +0300

    ornek

---
+++ ornek.js
@@ -0,0 +1,16 @@
+
+
+let cars=['BMW','Mercedes','Audio'];
+
+//for(let i=0;i<cars.length;i++){
+//console.log(cars[i]);
+//}
+
+//for(let i in cars){
+//    console.log(`index:${i},value:${cars[i]}`);
+//}
+
+//OBJECT
+cars.forEach(function(item){
+    console.log(item);
+})
