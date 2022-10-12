9cc28e78e7ccbddbed960fde6360af56277c55b3
Nisa <nisayurdagul385@gmmail.com>
2022-10-12 13:43:14 +0300

    window object

---
+++ windowNesnesi.js
@@ -0,0 +1,31 @@
+let val;
+
+/*var a=10;
+
+function b(){
+    return 0;
+}
+*/
+
+
+//ALERT
+alert('Merhaba');
+
+
+//prompt
+var c=prompt('bir sayi giriniz');
+console.log(c);
+
+
+//confirm
+var d=confirm('emin misin');
+console.log(d);
+
+
+
+//location
+//locatio.href-hostname-host-protocol
+val=window.location;
+console.log(val);
+
+// DOM-Document Object Model
