1c3449ba74e22f1f001a7915178c87f410edf7df
Nisa <nisayurdagul385@gmmail.com>
2022-10-12 13:51:34 +0300

    DOM

---
+++ DOM.js
@@ -0,0 +1,16 @@
+//Document Object Model-DOM
+
+let val;
+
+val=window.document;
+
+val=document.all;
+val=document.all.lengt;
+val=document.head;
+val=document.body;
+val=document.URL;
+val=document.images;
+val=document.title;
+val=document.forms;
+
+console.log(val);
